ec2c914aa3479c233dbdd855488a4ef3fa5dd966
monkeyz <sangmin3285@gmail.com>
2021-07-01 12:09:04 +0900

    first commit

---
+++ accountapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from accountapp.views import hello_world
+
+# 나중에 라우팅을 편하게 해주는 것을 하는 것
+app_name = 'accountapp'
+
+
+
+urlpatterns = [
+    path('hello_World/', hello_world, name='hello world'),
+]
---
+++ accountapp/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+
+# 1. 간단히 뷰를 먼저 만드는데
+# 1-1 함수를 만들자
+def hello_world(request):
+    return HttpResponse('Hello World')
+# 응답을 되돌려 주는 건데 그냥 응답을 되돌려준거 뿐이야
+
+# 2. 라우팅을 해줘야지 -> 어떻게 만드냐 -> urls에서 한다는 거야
