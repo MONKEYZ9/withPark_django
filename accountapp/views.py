50773a4ce06e3beeb48e86a1e6324fffa08e800e
monkeyz <sangmin3285@gmail.com>
2021-07-05 11:31:12 +0900

    add footer, header, head.html

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,8 +6,9 @@ from django.shortcuts import render
 
 # 1. 간단히 뷰를 먼저 만드는데
 # 1-1 함수를 만들자
-def hello_world(request):
-    return render(request, 'base.html')
 # 응답을 되돌려 주는 건데 그냥 응답을 되돌려준거 뿐이야
-
 # 2. 라우팅을 해줘야지 -> 어떻게 만드냐 -> urls에서 한다는 거야
+
+def hello_world(request):
+    return render(request, 'accountapp/hello_world.html')
+
