2496ed009dde6a08e3117b36b017321595444eed
monkeyz <sangmin3285@gmail.com>
2021-07-08 12:08:32 +0900

    add model & schema

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# models.Model 상속을 받을 거임
+# 새로운 디비안에 내용을 만들거야
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255) #이게 varchar랑 같은 거
