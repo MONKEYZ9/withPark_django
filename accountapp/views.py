2a616e74fb402bfd89a93bdb2ce3a2e523a3d749
monkeyz <sangmin3285@gmail.com>
2021-07-12 12:17:56 +0900

    modified view.py to show model's objects

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
 
 # 1. 간단히 뷰를 먼저 만드는데
@@ -17,15 +16,19 @@ from accountapp.models import HelloWorld
 def hello_world(req):
     if req.method == 'POST':
         temp = req.POST.get("input_text")
-        new_hello_world = HelloWorld() # 모델에서 가져왔어
+        new_hello_world = HelloWorld()  # 모델에서 가져왔어
         new_hello_world.text = temp
-        new_hello_world.save() # 새로운 헬로 월드라는게 만들어 졌는데 이걸 객체를 보내줄거라는거야
+        new_hello_world.save()  # 새로운 헬로 월드라는게 만들어 졌는데 이걸 객체를 보내줄거라는거야
 
+        new_hello_world_list = HelloWorld.objects.all()  # 여러가지가 있다. objects를 가져와야 하는데 여러가지가 있는 편이다.
+        # 이걸 다 보내주는 거야
 
         return render(req, 'accountapp/hello_world.html',
-                    context={'new_hello_world': new_hello_world})
+                      context={'new_hello_world': new_hello_world, 'new_hello_world_list': new_hello_world_list})
     else:
+        new_hello_world_list = HelloWorld.objects.all()  # 여러가지가 있다. objects를 가져와야 하는데 여러가지가 있는 편이다.
+        # post와 똑같이 리스트로 받아서 보여주도록 하자
+
         # 홈페이지에 접근할때는 보통 get 방식을 쓴다.
         return render(req, 'accountapp/hello_world.html',
-                    context={'text':'GET METHOD'})
-
+                      context={'new_hello_world_list': new_hello_world_list})
