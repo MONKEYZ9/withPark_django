343d6ef5b4c5fb3f92cea92cbc867f5ebcc4296c
monkeyz <sangmin3285@gmail.com>
2021-08-23 12:39:05 +0900

    add Field Lookups, update view, urls, list.html

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 
@@ -30,3 +31,22 @@ class SubscriptionView(RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
         return reverse('projectapp:detail', kwargs={'pk':kwargs['project_pk']})
+
+
+@method_decorator(login_required, 'get')
+class SubscriptionListView(ListView):
+    # 글을 아티클 글을 가져올거니까
+    model = Article # 이렇게 하면 내가 구독한 거 말고도 다 보여줘
+    context_object_name = 'article_list'
+    template_name = 'subscribeapp/list.html'
+    paginate_by = 20
+
+    # def get_context_data(self, *, object_list=None, **kwargs):
+    # 이걸 안쓰는 이유는 모든 것을 가져오는 것이다.
+    # 아래는 원하는 것만 골라서 쓸 수 있다.
+    def get_queryset(self):
+        # Subscription이 유저와 프로젝트를 연결해놨어               project만 리스트로 저장하겠다.
+        project_list = Subscription.objects.filter(user=self.request.user).values_list('project')
+        # 구독중인 리스트 안에 있는 글을 필터링을 해준다는 거야
+        article_list = Article.objects.filter(project__in=project_list)
+        return article_list
